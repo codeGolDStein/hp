22029b3dae97f9c7cd111ab095110f40e9c5b06c
kdennn <46850637+kdennn@users.noreply.github.com>
2025-05-01 11:49:33 +0000

    Woah test

---
+++ uebung01/ex-A1-ii.ino
@@ -0,0 +1,25 @@
+// Wait time in ms 1000ms = 1s
+unsigned long BLINK_TIME_MS_INTERVAL = 1000;
+// previous time 
+unsigned long previousMILLIS = 0;
+// set output pin
+#define LED_PIN 10
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMILLIS = milis();
+
+  if (currentMILLIS - previousMILLIS == BLINK_TIME_MS_INTERVAL){
+    previousMILLIS = currentMILLIS; // Reset
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN))
+    //Serial.print(digitalRead(LED_PIN))
+  }
+}
+
+// Aufgabe 1 ii) Da millis() die Milisekunden nach einem Start oder Reset zählt lässt sich damit präzieser und variabler einstellen lässt.
+// kann in der zwischen Zeit unser code weiter laufen. Bei delay() haltet der gesamte code bis der delay fertig ist.
