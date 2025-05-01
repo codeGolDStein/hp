18d6d51889b0a39500681022b37b1c81877662af
omar <omarhegab19551967@gmail.com>
2025-05-01 15:11:14 +0200

    blatt03
    gerne ihr pennise

--- uebung01/ex01-A3/ex01-A3.ino
+++ uebung01/ex01-A3/ex01-A3.ino
@@ -1,4 +1,3 @@
-// Include LCD functions:
 #include <LiquidCrystal.h>
 
 // LCD Connection
@@ -9,36 +8,34 @@
 #define DB6 12
 #define DB7 13
 
-// LCD Defines
 #define NUM_CHAR 20
 #define NUM_LINES 4
 
 #define INPUT_PIN A0
-#define REFERENCE_VOLTAGE 3.3 // assumes 3.3V
+#define REFERENCE_VOLTAGE 3.3
 
-// Global variables
-// Define the LCD screen
 LiquidCrystal lcd(R_S, E, DB4, DB5, DB6, DB7);
 
 void setup() {
-  // LCD has 4 lines with 20 chars
   lcd.begin(NUM_CHAR, NUM_LINES);
 }
 
-
 void loop() {
-  // Set cursor to arbitrary position
   int a0_value = analogRead(INPUT_PIN);
-  // convert
-  int voltage = (float) (a0_value * REFERENCE_VOLTAGE / 1023.0);
+
+  float voltage = (float)(a0_value * REFERENCE_VOLTAGE / 1023.0);
+
+  int volts = (int)voltage;
+  int dezimal = (int)((voltage - volts) * 100);  
 
   lcd.setCursor(0, 0);
+  lcd.print("Analog 0: ");
+  lcd.print(volts);
+  lcd.print(",");
+  if (dezimal < 10) lcd.print("0"); 
+  lcd.print(dezimal);
+  lcd.print("V");
 
-  // Print statements
-  lcd.print("Analog A0: " + String(voltage));
-
-  // Prevent display flickering for too fast updates
   delay(1000);
-  // Clear the display.
   lcd.clear();
 }
