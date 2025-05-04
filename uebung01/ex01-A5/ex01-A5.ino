af525f5faa1accb2b5fa85a55a6ce96fc469aaf5
omar <omarhegab19551967@gmail.com>
2025-05-04 18:19:57 +0200

    aufgabe 5

--- uebung01/ex01-A5/ex01-A5.ino
+++ uebung01/ex01-A5/ex01-A5.ino
@@ -11,31 +11,44 @@
 #define NUM_CHAR 20
 #define NUM_LINES 4
 
-#define INPUT_PIN A0
-#define REFERENCE_VOLTAGE 3.3
+#define INPUT_PIN A1
 
+// LCD initialisieren
 LiquidCrystal lcd(R_S, E, DB4, DB5, DB6, DB7);
+
+// Merken, was zuletzt angezeigt wurde
+String lastButton = "";
 
 void setup() {
   lcd.begin(NUM_CHAR, NUM_LINES);
+  lcd.setCursor(0, 1);  // Start auf zweiter Zeile
+  lcd.print("button: -");
 }
 
 void loop() {
-  int a0_value = analogRead(INPUT_PIN);
+  int a1_value = analogRead(INPUT_PIN);
+  String currentButton;
 
-  float voltage = (float)(a0_value * REFERENCE_VOLTAGE / 1023.0);
+  if (a1_value >= 0 && a1_value <= 100) {
+    currentButton = "S1";
+  } else if (a1_value >= 101 && a1_value <= 200) {
+    currentButton = "S2";
+  } else if (a1_value >= 201 && a1_value <= 300) {
+    currentButton = "S3";
+  } else if (a1_value >= 301 && a1_value <= 400) {
+    currentButton = "S4";
+  } else if (a1_value >= 401 && a1_value <= 500) {
+    currentButton = "S5";
+  } else {
+    currentButton = "-";
+  }
 
-  int volts = (int)voltage;
-  int dezimal = (int)((voltage - volts) * 100);  
+  // Nur neu anzeigen, wenn sich der Status ändert
+  if (currentButton != lastButton) {
+    lcd.setCursor(0, 1);  // Zweite Zeile
+    lcd.print("button: " + currentButton + "       ");
+    lastButton = currentButton;
+  }
 
-  lcd.setCursor(0, 0);
-  lcd.print("Analog 0: ");
-  lcd.print(volts);
-  lcd.print(".");
-  if (dezimal < 10) lcd.print("0"); 
-  lcd.print(dezimal);
-  lcd.print("V");
-
-  delay(1000);
-  lcd.clear();
+  delay(200);
 }
