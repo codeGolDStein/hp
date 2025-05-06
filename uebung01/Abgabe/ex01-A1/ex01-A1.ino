0dd6e6be7cc0c989eef042a1c3ab60495da13b42
omar <omarhegab19551967@gmail.com>
2025-05-06 15:14:52 +0200

    penis

--- uebung01/Abgabe/ex01-A1/ex01-A1.ino
+++ uebung01/Abgabe/ex01-A1/ex01-A1.ino
@@ -1,9 +1,15 @@
+// Aufgabe 1 ii) Da millis() die Milisekunden nach einem Start oder Reset zählt lässt sich damit präzieser und variabler einstellen lässt.
+// kann in der zwischen Zeit unser code weiter laufen. Bei delay() haltet der gesamte code bis der delay fertig ist.
+
+//Aufgabe 1 i)
+// Wait time in ms 1000ms = 1s
+#define BLINK_TIME_MS 1000
+// set output pin
+#define LED_PIN 10
 // Wait time in ms 1000ms = 1s
 unsigned long BLINK_TIME_MS_INTERVAL = 1000;
 // previous time 
 unsigned long previousMILLIS = 0;
-// set output pin
-#define LED_PIN 10
 // set LED Status
 bool LED_STATUS = false;
 
@@ -13,6 +19,16 @@ void setup() {
 }
 
 void loop() {
+
+  /*
+  // Aufgabe 1 i)
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, HIGH);// Set LED on
+  delay(BLINK_TIME_MS); // Wait
+  digitalWrite(LED_PIN, LOW); //Set LED off
+  delay(BLINK_TIME_MS); // Wait
+  */
+
   unsigned long currentMILLIS = millis();
 
   if (currentMILLIS - previousMILLIS >= BLINK_TIME_MS_INTERVAL){
@@ -28,6 +44,3 @@ void loop() {
 
   }
 }
-
-// Aufgabe 1 ii) Da millis() die Milisekunden nach einem Start oder Reset zählt lässt sich damit präzieser und variabler einstellen lässt.
-// kann in der zwischen Zeit unser code weiter laufen. Bei delay() haltet der gesamte code bis der delay fertig ist.
