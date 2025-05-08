5631f87f3e1eca613dbcef3c74e6123424e0e3a3
kdennn <46850637+kdennn@users.noreply.github.com>
2025-05-08 17:30:25 +0200

    cocklingo

--- uebung02/uebung02.ino
+++ uebung02/uebung02.ino
@@ -1,6 +1,6 @@
 // Pinbelegung
-const int A1 = D1;  // GPIO5
-const int A2 = D2;  // GPIO4
+const int8_t A1 = D1;  // GPIO5
+const int8_t A2 = D2;  // GPIO4
 
 void setup() {
   // Pins als Ausgang definieren
@@ -12,7 +12,17 @@ void setup() {
   digitalWrite(A2, LOW);
 }
 
-// Funktion zum Steuern des Motors
+void loop() {
+  // Beispiel: Vorwärts für 2 Sekunden
+  setMotor(true);
+  delay(2000);
+
+  // Rückwärts für 2 Sekunden
+  //setMotor(false);
+  delay(2000);
+}
+
+//Aufgabe 1, Funktion zum Steuern des Motors
 void setMotor(bool forward) {
   if (forward) {
     digitalWrite(A1, LOW);   // A1 = 0
@@ -21,14 +31,4 @@ void setMotor(bool forward) {
     digitalWrite(A1, HIGH);  // A1 = 1
     digitalWrite(A2, LOW);   // A2 = 0
   }
-}
-
-void loop() {
-  // Beispiel: Vorwärts für 2 Sekunden
-  setMotor(true);
-  delay(2000);
-
-  // Rückwärts für 2 Sekunden
-  setMotor(false);
-  delay(2000);
 }
