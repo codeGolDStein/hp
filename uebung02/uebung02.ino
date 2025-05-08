07831ca3ff2ff7cab19ee9807b08e091de18f3f6
kdennn <46850637+kdennn@users.noreply.github.com>
2025-05-08 18:01:29 +0200

    Update uebung02.ino

--- uebung02/uebung02.ino
+++ uebung02/uebung02.ino
@@ -1,20 +1,22 @@
 // Pinbelegung
-const int8_t A1 = D1;  // GPIO5
-const int8_t A2 = D2;  // GPIO4
+const int8_t A1_PIN = D1;  // GPIO5
+const int8_t A2_PIN = D2;  // GPIO4
+const int8_t B1_PIN = D5;
+const int8_t B2_PIN = D6;
+
 
 void setup() {
   // Pins als Ausgang definieren
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
+  pinMode(A1_PIN, OUTPUT);
+  pinMode(A2_PIN, OUTPUT);
+  pinMode(B1_PIN, OUTPUT);
+  pinMode(B2_PIN, OUTPUT);
   
-  // Optional: Motor am Anfang stoppen
-  digitalWrite(A1, LOW);
-  digitalWrite(A2, LOW);
 }
 
 void loop() {
   // Beispiel: Vorwärts für 2 Sekunden
-  setMotor(true, 100);
+  setMotor(true, 300, true);
   delay(2000);
 
   // Rückwärts für 2 Sekunden
@@ -22,14 +24,22 @@ void loop() {
   //delay(2000);
 }
 
-// Neue Funktion mit Geschwindigkeitskontrolle
-void setMotor(bool forward, uint16_t speed) {
+// Neue Funktion mit Geschwindigkeitskontrolle (motor = true -> right Wheel)
+void setMotor(bool forward, uint16_t speed, bool motor) {
+  
+  int8_t Motor1 = B1_PIN;
+  int8_t Motor2 = B2_PIN;
+
+  if (!motor){
+    Motor1 = A1_PIN;
+    Motor2 = A2_PIN;
+  }
 
   if (forward) {
-    analogWrite(A1, 0);         // LOW
-    analogWrite(A2, speed);     // PWM Signal
+    analogWrite(Motor1, 0);
+    analogWrite(Motor2, speed);
   } else {
-    analogWrite(A1, speed);     // PWM Signal
-    analogWrite(A2, 0);         // LOW
+    analogWrite(Motor1, speed);
+    analogWrite(Motor2, 0);
   }
 }
