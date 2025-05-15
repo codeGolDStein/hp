7cc65308cd75475e420f2a9e9119b4cf121e53d4
omar <omarhegab19551967@gmail.com>
2025-05-15 19:11:28 +0200

    Alex fick dich

--- uebung03/uebung03.ino
+++ uebung03/uebung03.ino
@@ -1,17 +1,3 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdbool.h>
-
-// Ex03: Aufgabe 1 – Direktes Setzen von Pin 13 über PORTB Bit 5 (PB5)
-
-void setPin13(bool high) {
-    if (high) {
-        PORTB |= (1 << PB5);    // Setze Bit 5 (Pin 13 high)
-    } else {
-        PORTB &= ~(1 << PB5);   // Lösche Bit 5 (Pin 13 low)
-    }
-}
-
 void setup() {
     // Setze Pin 13 als Ausgang (Data Direction Register B, Bit 5)
     DDRB |= (1 << PB5);
@@ -23,6 +9,14 @@ void loop() {
     _delay_ms(500);
     setPin13(false);
     _delay_ms(500);
+}
+
+void setPin13(bool high) {
+    if (high) {
+        PORTB |= (1 << PB5);    // Setze Bit 5 (Pin 13 high)
+    } else {
+        PORTB &= ~(1 << PB5);   // Lösche Bit 5 (Pin 13 low)
+    }
 }
 
 // Arduino-kompatibles Main für reine C-Umgebung
