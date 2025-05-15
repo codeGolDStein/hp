83a9f746add8926f462ac7d6a9b683d3e90a118c
omar <omarhegab19551967@gmail.com>
2025-05-15 20:28:48 +0200

    Alex du fetsack

--- uebung03/uebung03.ino
+++ uebung03/uebung03.ino
@@ -1,5 +1,6 @@
 #define OUTPUT_PIN PB4  // Arduino Pin 12 = PB4
 #define NOTE_C6 1046
+#define SOUND_PIN PB2   // Pin 10 = OC1B (for sound output)
 
 void setup() {
     // Setze Pin 13 als Ausgang (Data Direction Register B, Bit 5)
@@ -27,33 +28,52 @@ void setPin13(bool high) {
 void setTimer1Freq() {
     cli(); // Disable global interrupts during setup
 
+    /*
     // Set Pin 12 (PB4) as output
     DDRB |= (1 << OUTPUT_PIN);
+    */
 
+    // Set Pin 10 (PB2 = OC1B) as output
+    DDRB |= (1 << SOUND_PIN);
+
+    /*
     // Reset Timer1 registers
     TCCR1A = 0;
     TCCR1B = 0;
     TIMSK1 = 0;
+    */
+
+    // Reset Timer1 control registers
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1  = 0;
 
     // Set CTC mode (WGM12 = 1, others = 0)
     TCCR1B |= (1 << WGM12);
 
+     // Enable hardware toggle on OC1B (Pin 10)
+    TCCR1A |= (1 << COM1B0);  // Toggle OC1B on compare match
+
     // Set compare match value for 1046 Hz
     OCR1A = (F_CPU / (2 * 8 * NOTE_C6)) - 1;
 
+    /*
     // Enable Timer1 Compare A Match interrupt
     TIMSK1 |= (1 << OCIE1A);
-
+    */
+    
     // Set prescaler to 8 and start the timer
     TCCR1B |= (1 << CS11);  // CS11 = prescaler 8
 
     sei(); // Enable global interrupts
 }
 
+/*
 // ISR toggles PB4 (Pin 12)
 ISR(TIMER1_COMPA_vect) {
     PINB |= (1 << OUTPUT_PIN); // Toggle pin
 }
+*/
 
 // Arduino-kompatibles Main für reine C-Umgebung
 int main(void) {
